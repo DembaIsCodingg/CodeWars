5fad70026cfefed585427c6dab68672bc25e0149
Demba Soumare <dembawrld30@gmail.com>
2024-04-10 22:49:03 -0400

    Hello world

---
+++ helloworld.js
@@ -0,0 +1,13 @@
+// Write a function "greet" that returns "hello world!"
+//PREP
+//write a f => "Hello world!"
+//'Hello world' simple hello world
+
+function greet(){
+    //return hello world
+    
+    return 'hello world!'
+    
+  }
+  
+  greet() //'Hello World'
